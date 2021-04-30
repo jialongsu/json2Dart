e43c5416848b3a1e6fdbe7fb0dad6b847260ac50
Arno <jialong.su@aliyun.com>
2021-04-30 12:09:57 +0800

    修改根路由
    修改根路由

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "6dff225a377191a79ea4369e2c2fed7c",
-"index.html": "50c9c718ed431544733a91b3529e41db",
-"/": "50c9c718ed431544733a91b3529e41db",
+"index.html": "66db824e421787c74cd8c88a96aea235",
+"/": "66db824e421787c74cd8c88a96aea235",
 "main.dart.js": "34625f775c9561ec4d4a37c9e87789fe",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
